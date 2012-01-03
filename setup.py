2f21700a5940e1ec1a596a09d010f5609b17b083
Jonathan Soo <jcsoo@agora.com>
2012-01-02 21:50:17 -0500

    bump build number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
    name = 'orm',
-   version = '0.1',
+   version = '0.1-r0001',
    packages = find_packages(),
    test_suite = 'orm.tests'
 )
