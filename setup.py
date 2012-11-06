ae3efb1bb1c0ba4ea5d86e226a92067235991768
Jonathan Soo <jcsoo@agora.com>
2012-11-06 13:52:24 -0500

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
    name = 'orm',
-   version = '0.1-r0001',
+   version = '0.2',
    packages = find_packages(),
    test_suite = 'orm.tests',
    install_requires = ['psycopg2']
